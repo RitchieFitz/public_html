1e81d03b01d2ba15dd35330c026befc43c36bbf4
Ritchie Fitzgerald <ritchiefitz1@gmail.com>
2015-02-03 10:32:08 -0700

    Initial Commit

---
+++ js/assign03.js
@@ -0,0 +1,113 @@
+function $(selector) {
+	return document.querySelectorAll(selector);
+}
+
+function addClass(element, newClass) {
+	element.className = element.className + " " + newClass;
+}
+
+function removeClass(element, deleteClass) {
+	var reg = new RegExp(" " + deleteClass,"g");
+	element.className = element.className.replace(reg, '');
+}
+
+function playSong(button, song) {
+	addClass(button, "pause-button");
+	song.play();
+}
+
+function pauseSong(button, song) {
+	removeClass(button, "pause-button");
+	song.pause();
+}
+
+function pauseOtherSongs(currentButton) {
+	var checkButtons = $(".pause-button");
+
+	for (i = 0; i < checkButtons.length; i++) {
+		var button = checkButtons[i];
+		var song = checkButtons[i].previousElementSibling;
+
+		if (currentButton === button) continue;
+
+		if (contains(button.className, "pause")) {
+			removeClass(button.parentNode.parentNode, "playing");
+			pauseSong(button, song);
+		}
+	}
+}
+
+function fixNav() {
+	var timer;
+	document.addEventListener("scroll", function() {
+		clearTimeout(timer);
+		timer = setTimeout(checkScroll, 10)
+	});
+
+	var checkScroll = function () {
+		var scrollPosition = document.body.scrollTop;
+		var mainNav = $("#main-nav")[0];
+		var navClass = mainNav.className;
+
+		if (scrollPosition > 95 && !contains(navClass, "fixed")) {
+			addClass(mainNav, "fixed");
+		}
+		else if (scrollPosition < 95) {
+			removeClass(mainNav, "fixed");
+		}
+	};
+
+}
+
+function contains(str, substr) {
+	return str.indexOf(substr) > -1;
+}
+
+function setButtons() {
+	var playButtons = $(".play-button");
+
+	for (i = 0; i < playButtons.length; i++) {
+
+		if (playButtons[i].previousElementSibling) {
+
+			playButtons[i].addEventListener("click", function () {
+				var song = this.previousElementSibling;
+				var button = this;
+
+				pauseOtherSongs(button);
+
+				// console.log("I was clicked");
+
+				if (contains(button.className, "pause")) {
+					removeClass(button.parentNode.parentNode, "playing");
+					pauseSong(button, song);
+				}
+				else {
+					addClass(button.parentNode.parentNode, "playing");
+					playSong(button, song);
+				}
+			});
+		}
+	}
+}
+
+function setInternalAnchors() {
+	var anchors = $("#main-nav ul ul a");
+
+	for (i = 0; i < anchors.length; i++) {
+		anchors[i].addEventListener("click", function () {
+			setTimeout( function() {
+				console.log("Before: " + document.body.scrollTop);
+				document.body.scrollTop -= 50;
+				console.log("After: " + document.body.scrollTop);
+			}, 1);
+		});
+	}
+}
+
+function main() {
+
+	setButtons();
+	setInternalAnchors();
+	fixNav();
+}
---
+++ js/assign04.js
@@ -0,0 +1,224 @@
+/*************************************************************************
+ * $
+ * This lets me select any element by using a CSS query.
+ *************************************************************************/
+ function $(selector)
+ {
+ 	return document.querySelectorAll(selector);
+ }
+
+/*************************************************************************
+ * ADD CLASS
+ * This adds a class to the passed in element.
+ *************************************************************************/
+ function addClass(element, newClass)
+ {
+ 	element.className = element.className + " " + newClass;
+ }
+
+/*************************************************************************
+ * REMOVE CLASS
+ * This removes a class to the passed in element.
+ *************************************************************************/
+ function removeClass(element, deleteClass)
+ {
+ 	var reg = new RegExp(" " + deleteClass,"g");
+ 	element.className = element.className.replace(reg, '');
+ }
+
+/*************************************************************************
+ * CHANGE CONTENT
+ * This changes the html content of the passed in element.
+ *************************************************************************/
+ function changeContent(element, message)
+ {
+ 	element.innerHTML = message;
+ }
+
+/*************************************************************************
+ * CONTAINS
+ * This searches for a substring in string.
+ *************************************************************************/
+ function contains(str, substr)
+ {
+ 	return str.indexOf(substr) > -1;
+ }
+
+/*************************************************************************
+ * CLEAR CHECKS
+ * This removes all success and error classes from form. It also removes
+ * all messages letting the user start from the beginning.
+ *************************************************************************/
+ function clearChecks()
+ {
+	// This will select all enabled inputs.
+	var elements = $("form input[type*=text]:enabled");
+
+	// Remove success or error from each element.
+	for (i = 0; i < elements.length; i++)
+	{
+		if (contains(elements[i].className, "success"))
+		{
+			removeClass(elements[i], "success");
+		}
+		else if (contains(elements[i].className, "error"))
+		{
+			removeClass(elements[i], "error");
+		}
+	}
+
+	// This selectes all message elements.
+	var messages = $(".message");
+
+	// This will remove the content in each message element.
+	for (i = 0; i < messages.length; i++)
+	{
+		changeContent(messages[i], "");
+	}
+}
+
+/*************************************************************************
+ * INPUT STATUS
+ * This allows us to notify the user if their input was successful or not.
+ * Pass in the element to effect, type of message, and then the actual
+ * message.
+ *************************************************************************/
+ function inputStatus(node, type, message)
+ {
+	// This gets the message element that matches the input node.
+	var nodeMess = node.nextSibling.nextSibling;
+
+	// Add error class or success class if it wasn't already set.
+	// Change only if messages are different for error class.
+	if (type == "error" && message != nodeMess.innerHTML)
+	{
+		// Remove success class if it exists.
+		if (contains(node.className, "success"))
+		{
+			removeClass(node, "success-input");
+			removeClass(nodeMess, "success");
+		}
+
+		addClass(node, "error-input");
+		addClass(nodeMess, "error");
+		changeContent(nodeMess, message);
+	}
+	else if (type == "success" && !contains(node.className, "success"))
+	{
+		// Remove error class if it exists.
+		if (contains(node.className, "error"))
+		{
+			removeClass(node, "error-input");
+			removeClass(nodeMess, "error");
+		}
+
+		addClass(node, "success-input");
+		addClass(nodeMess, "success");
+		changeContent(nodeMess, message);
+	}
+
+	// Position message element correctly.
+	nodeMess.style.right = (nodeMess.offsetWidth * -1) - 20 + "px";
+
+	setTimeout( function()
+	{
+		nodeMess.style.right = "-" + (nodeMess.offsetWidth + 20) + "px";
+	}, 10);
+}
+
+/*************************************************************************
+ * UPDATE
+ * This checks the inputs and make sure they are not empty or contain any
+ * errors before updating.
+ *************************************************************************/
+ function update() {
+	// Input Values
+	var aprV = $("#apr")[0].value;
+	var termV = $("#loan-term")[0].value;
+	var amountV = $("#loan-amount")[0].value;
+
+	// Input Classes
+	var aprCN = $("#apr")[0].className;
+	var termCN = $("#loan-term")[0].className;
+	var amountCN = $("#loan-amount")[0].className;	
+
+	// Check if fields are empty.
+	if (aprV != "" && termV != "" && amountV != "")
+	{
+		// Check if any fields contain any errors.
+		if (!contains(aprCN, "error") && !contains(termCN, "error") && !contains(amountCN, "error"))
+		{
+			calculate(aprV, termV, amountV);
+		}
+		else
+		{
+			$("#payment")[0].value = "Fix Errors";
+		}
+	}
+	else
+	{
+		$("#payment")[0].value = "Fill in fields";
+	}
+}
+
+/*************************************************************************
+ * CHECK INPUT
+ * This validates the input field, and will update the input status
+ * accordingly.
+ *************************************************************************/
+ function checkInput(element)
+ {
+	// Make sure the value is a numeric value, and that it is not
+	// empty.
+	if (isNaN(element.value) == true)
+	{
+		inputStatus(element, "error", "Error: contains non-digit");
+	}
+	else if (element.value == "")
+	{
+		inputStatus(element, "error", "Error: cannot leave empty");
+		element.focus();
+	}
+	else {
+		inputStatus(element, "success", "Success");
+	}
+
+	// This will update the monthly payment if all fields are filled in
+	// and don't contain any errors.
+	update();
+}
+
+/*************************************************************************
+ * CALCULATE
+ * This will calculate the monthly payment and update the disabled text
+ * field.
+ *************************************************************************/
+ function calculate(apr, term, amount)
+ {
+ 	var monthlyPayment = 0;
+ 	var numPayments = term * 12;
+ 	var mRate = (apr/100) / 12;
+ 	var tmp = Math.pow(1 + mRate, numPayments);
+
+ 	if (mRate != 0)
+ 	{
+ 		monthlyPayment = amount * (mRate * tmp / (tmp - 1));
+ 	}
+ 	else
+ 	{
+ 		monthlyPayment = amount / numPayments;
+ 	}
+
+ 	monthlyPayment = monthlyPayment.toFixed(2);
+ 	$("#payment")[0].value = "$" + monthlyPayment;
+ }
+
+/*************************************************************************
+ * MAIN
+ * Run when page loads.
+ *************************************************************************/
+function main()
+{
+	// Set focus on APR when page loads.
+	$("#apr")[0].focus();
+}
